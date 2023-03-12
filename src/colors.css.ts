a38c89e344015996f367b92ee70ee859339ec81d
matkl1 <mk@medieninformatik.berlin>
2023-03-12 01:01:37 +0100

    colors

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,14 @@
 export const colors = {
-  background: "#f03e2f",
+  /*background: "#f03e2f",
   text: "#000000",
   primary: "#000000",
   muted: "#f14e41",
   active: "#e6e6e6",
+  black: "#000",*/
+  background: "#ccc",
+  text: "#000000",
+  primary: "#000000",
+  muted: "#aaa",
+  active: "#00ff00",
   black: "#000",
 }
